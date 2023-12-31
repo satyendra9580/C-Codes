864bbff7a5a08d309e18dec56819336c26cc6097
satyendra9580 <seeny5180singh@gmail.com>
2023-12-31 12:36:33 +0530

    Largest element in the array

--- largestelementinarray.cpp
+++ largestelementinarray.cpp
@@ -1,2 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+int largestelement(int arr[], int n){
+    int largest=arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    return largest;
+}
+int main(){
+     int n;
+     cout<<"Enter the size of an array: ";
+     cin>>n;
+     int arr[n];
+     for(int i=0; i<n; i++){
+        cin>>arr[i];
+     }
+     int result=largestelement(arr,n);
+     cout<<"Largest element is: "<<result;
+    return 0;
+}

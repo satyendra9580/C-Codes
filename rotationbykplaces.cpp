1367a0dd481c0b0baa5b74ccc02c9c6b3d0fbd60
satyendra9580 <seeny5180singh@gmail.com>
2024-01-04 14:00:53 +0530

    Left Rotation by k places in array

---
+++ rotationbykplaces.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int rotation(int arr[], int n, int k){
+    int temp[k];
+    for(int i=0; i<k; i++){
+        temp[i]=arr[i];
+    }
+    for(int i=k; i<n; i++){
+        arr[i-k]=arr[i];
+    }
+    for(int i=n-k; i<n; i++){
+        arr[i]=temp[i-(n-k)];
+    }
+}
+int main(){
+    int arr[]={5,4,7,9,6,1,3};
+    rotation(arr,7,3);
+    for(int i=0; i<7; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}

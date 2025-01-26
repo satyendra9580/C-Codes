a5cf9a9bb6d7278513e0c37f9606b68ff3ba1d8e
Satyendra singh <seenu5180singh@gmail.com>
2025-01-27 02:09:58 +0530

    cf problem

---
+++ plus_minus.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(c==(a+b)) cout<<'+'<<endl;
+        else cout<<'-'<<endl;
+    }
+    return 0;
+}

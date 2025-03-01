7b1610c065a4a3373da9db05211cb9708b2654da
Satyendra singh <seenu5180singh@gmail.com>
2025-03-02 02:13:27 +0530

    cf problem

---
+++ Mocha_and_Math.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)cin>>a[i];
+    int res=a[0];
+    for(int i=1;i<n;i++)res&=a[i];
+    cout<<res<<endl;
+    }
+    return 0;
+}

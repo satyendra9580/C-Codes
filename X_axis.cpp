f78e99dccd8c3f9d25410b07437811abaaccdc78
Satyendra singh <seenu5180singh@gmail.com>
2025-06-09 19:54:31 +0530

    cf problem

---
+++ X_axis.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x1,x2,x3;
+    cin>>x1>>x2>>x3;
+
+    int maxi=max({x1,x2,{x3}});
+    int mini=min({x1,x2,{x3}});
+
+    cout<<(maxi-mini)<<endl;
+    }
+    return 0;
+}
